e2a0afbf60710eaf38c637202ec97842f45b4b00
MyMilkedEek <eek@mymilkedeek.net>
2015-04-18 11:19:24 +0200

    player is being rendered

---
+++ core/src/net/mymilkedeek/ludum32/actors/Player.java
@@ -0,0 +1,24 @@
+package net.mymilkedeek.ludum32.actors;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * 18/04/2015 - 11:14
+ *
+ * @author Michaël
+ */
+public class Player extends Actor {
+
+    private final String TAG = "net.mymilkedeek.ludum32.actors.Player";
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+}
