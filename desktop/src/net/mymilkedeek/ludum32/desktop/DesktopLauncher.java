d5c2cf77dbe1af8b03f659c7b92e3da8cd9461eb
MyMilkedEek <eek@mymilkedeek.net>
2015-04-18 10:41:47 +0200

    initial commit

---
+++ desktop/src/net/mymilkedeek/ludum32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mymilkedeek.ludum32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mymilkedeek.ludum32.GameInstance;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameInstance(), config);
+	}
+}
